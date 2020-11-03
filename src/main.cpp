28d0536fdbf7d12f7473c048666a4f388355df67
CanadianBacon5243 <38812747+CanadianBacon5243@users.noreply.github.com>
2020-11-03 12:28:30 -0600

    Added CMake and started file system

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by buddm on 11/3/2020.
+//
+
+#include "main.h"
+
+int main(){
+
+	return 0;
+}
---
+++ src/main.h
@@ -0,0 +1,8 @@
+//
+// Created by buddm on 11/3/2020.
+//
+
+#ifndef ENGINE_ALPHA_MAIN_H
+#define ENGINE_ALPHA_MAIN_H
+
+#endif //ENGINE_ALPHA_MAIN_H
