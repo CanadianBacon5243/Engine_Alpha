ff2df31e6eb74eba6e5418f1c85ebf4917b4df36
CanadianBacon5243 <38812747+CanadianBacon5243@users.noreply.github.com>
2020-11-03 20:32:33 -0600

    Creates blank window

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,41 @@
 //
 // Created by buddm on 11/3/2020.
 //
-
+#include "src/Engine_AlphaConfig.h"
 #include "main.h"
 
-int main(){
 
+int main(int argc, char* argv[]){
+	versionPrint(argv);
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "main.cpp", nullptr, nullptr);
+	if (window == nullptr){
+		fprintf(stderr, "Failed to create GLFW window\n");
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+		fprintf(stderr, "Failed to initialize GLAD\n");
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	while (!glfwWindowShouldClose(window)){
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 	return 0;
 }
--- src/main.h
+++ src/main.h
@@ -4,5 +4,15 @@
 
 #ifndef ENGINE_ALPHA_MAIN_H
 #define ENGINE_ALPHA_MAIN_H
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 
+void versionPrint(char* argv[]){
+	std::cout << argv[0] << " Version " << Engine_Alpha_VERSION_MAJOR << "." << Engine_Alpha_VERSION_MINOR << '\n';
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height){
+	glViewport(0, 0, width, height);
+}
 #endif //ENGINE_ALPHA_MAIN_H
